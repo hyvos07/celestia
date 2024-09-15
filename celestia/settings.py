8500bd2f6d1466beca36bc0078cdf313eafb24ba
Hyvos <daniel.liman07@gmail.com>
2024-09-16 00:43:56 +0700

    ci: Deploy

--- celestia/settings.py
+++ celestia/settings.py
@@ -132,3 +132,10 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost",
+    "http://127.0.0.1",
+    "http://daniel-liman-celestia.pbp.cs.ui.ac.id", 
+    "https://daniel-liman-celestia.pbp.cs.ui.ac.id",
+]
