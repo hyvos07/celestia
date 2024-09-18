3f9605ebe4f254240321a2c1e03166ea8d977105
Hyvos <daniel.liman07@gmail.com>
2024-09-18 10:56:37 +0700

    ci: Redeploy without Debug Status

--- celestia/settings.py
+++ celestia/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-he8svpxvhhscak4!7p!h1+3l-%)6(u=_1-zlyv#mpb@0nui_+u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "daniel-liman-celestia.pbp.cs.ui.ac.id"]
 
