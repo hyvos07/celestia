aa8528a145b075edb9c568feafef21059885bc0f
Hyvos <daniel.liman07@gmail.com>
2024-09-18 10:46:47 +0700

    build: Rebuild sqlite migration file

--- celestia/settings.py
+++ celestia/settings.py
@@ -126,11 +126,7 @@ STATICFILES_DIRS = [
 # Add a directory where static files will be collected
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
-STORAGES = {
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
-}
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
